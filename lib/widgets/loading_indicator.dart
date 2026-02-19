0f2e5788515adb3024a11e5bc5fa3bb24f8cbb65
frolidanicas <frolidanicas@gmail.com>
2026-02-18 17:37:16 -1200

    creading loaading indicator widget

---
+++ lib/widgets/loading_indicator.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  final double size;
+  final Color? color;
+
+  const LoadingIndicator({
+    super.key,
+    this.size = 48,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(
+          color ?? Theme.of(context).colorScheme.primary,
+        ),
+      ),
+    );
+  }
+}
